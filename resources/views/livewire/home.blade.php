df947b12539b83a3cb2a982d1bf15e9b3df7d5c4
mtinsbanj <mtinsbanj@gmail.com>
2025-04-10 09:11:55 +0100

    api_key update

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -67,7 +67,7 @@
                     {{ $add['formatted_address'] }}
                   </option>
                   @endforeach
-                </select>
+                </select> 
               </div>
               @endif
 
